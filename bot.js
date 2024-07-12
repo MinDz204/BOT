01bc89227064d136fc4432693e186fb78ed548f9
Ziji <Zijipia@gmail.com>
2024-07-12 20:45:06 +0700

    p -> play

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const { Client, GatewayIntentBits, Partials, Collection, Events } = require("dis
 const config = require("./config.js");
 const fs = require("fs");
 const { Player } = require('discord-player');
-const { YoutubeiExtractor } = require("discord-player-youtubei")
 
 const client = new Client({
   partials: [
@@ -57,10 +56,6 @@ const player = new Player(client, {
 });
 
 player.setMaxListeners(200);
-// player.extractors.register(YoutubeExtractor, {
-//   createStream: createYoutubeiStream
-// })
-player.extractors.register(YoutubeiExtractor, {})
 player.extractors.loadDefault()
 
 // player.on("debug", console.log)
@@ -145,4 +140,3 @@ process.on('uncaughtException', (error) => {
   client.errorLog?.send(`**${config?.Zmodule}** <t:${Math.floor(Date.now() / 1000)}:R>\n${error?.stack}`)
   console.error('Uncaught exception:', error);
 });
-
